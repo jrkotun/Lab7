c6da7224555ebcd74cea077d0228b193a40f6c13
Chelsey Long <chelseylong22@gmail.com>
2015-03-03 15:39:06 -0600

    Got it working CL

--- RombergIntegration.cpp
+++ RombergIntegration.cpp
@@ -5,31 +5,31 @@
 using CSC2110::Double;
 
 #include <math.h>
+#include <iostream>
+using namespace std;
 
 //a is the lower limit and b is the upper limit
 double RombergIntegration::accurateRomberg(MultiVarFunction* f, double a, double b, int level)
 {
    Double* db;  //use this variable to place and retrieve values on the queue
    
-   QueueLinked<Double>* q1 =
-   QueueLinked<Double>* q2 =
+   QueueLinked<Double>* q1 = new QueueLinked<Double>();		//I DID
+   QueueLinked<Double>* q2 = new QueueLinked<Double>();
 
 
    int counter = 0;
    int n = 1;  //current number of intervals
-   while (                     )
+   while ( counter <= level)  //I did 
    {
       //DO THIS
       //obtain the required number of trapezoid evaluations depending on the number of levels requested
       //put all of the level 0 results on the q1
 
 
+      double integ = RecursiveIntegration::romberg(f, a, b, n);
 
-
-
-
-
-
+	  Double* num = new Double(integ);
+	  q1->enqueue(num);
 
 
       n = 2*n;  //double the number of intervals
@@ -45,31 +45,58 @@ double RombergIntegration::accurateRomberg(MultiVarFunction* f, double a, double
    //the total number of executions of the loop is ??
 
    //DO THIS
-   int iterations =                //can be precomputed
+   int alias = level;
+   int iter;
+   while(alias>0)
+   {
+		iter += alias;
+		alias--;
+   }
+   int iterations = iter;	 //can be precomputed I DID
    while (iterations > 0)
    {
       //DO THIS
       //use the algorithm described in the lab to improve the accuracy of your level 0 results
 
+	for(int i = q1->size()-1; i > 0; i--)
+	{	
+   	  Double* il = q1->dequeue();
+	  Double* im = q1->peek();
+	  
+	  factor = ((pow(4, power) * im->getValue())- il->getValue())/(pow(4, power)-1);
+	  
+	  delete il;
+	  
+	  Double* calc = new Double(factor);
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+	  q2->enqueue(calc);
+	  
+	}
+	
+	QueueLinked<Double>* temp = new QueueLinked<Double>();
+	
+	int size = q2->size();
+	for(int i = 1; i <= size; i++)
+	{
+		if (q2->size() == 1)
+		{
+			Double* item = q2->dequeue();
+			temp->enqueue(item);
+			delete item;
+			q1 = temp;
+			delete q2;
+		}
+		else
+		{
+		Double* item = q2->dequeue();
+		temp->enqueue(item);
+		delete item;
+		}
+	}
 
       iterations--;
+	  power++;
    }
 
    //obtain the final answer
